28041d3a32a49c75de500736869d9ac62622cb41
vartanbeno <vartanbeno@gmail.com>
2018-07-26 17:36:16 -0400

    set up express server

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const PORT = 3000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send('Hello from server!')
+})
+
+app.listen(PORT, () => {
+    console.log('Server running on localhost:' + PORT);
+})
