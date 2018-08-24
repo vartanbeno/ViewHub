9f764cc8619a598d46cac805511d9b10e47ebdf5
vartanbeno <vartanbeno@gmail.com>
2018-08-23 20:20:31 -0400

    #51 fix HomeComponent tests, add one for Welcome message

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,14 +1,41 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { AddPostComponent } from '../add-post/add-post.component';
+import { ListOfPostsComponent } from '../list-of-posts/list-of-posts.component';
+import { LoaderComponent } from '../loader/loader.component';
+import { ErrorMessageComponent } from '../error-message/error-message.component';
+import { EditPostComponent } from '../edit-post/edit-post.component';
+import { DeletePostComponent } from '../delete-post/delete-post.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let debugElement: DebugElement;
+  let htmlElement: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [
+        HomeComponent,
+        AddPostComponent,
+        ListOfPostsComponent,
+        LoaderComponent,
+        ErrorMessageComponent,
+        EditPostComponent,
+        DeletePostComponent
+      ],
+      imports: [
+        FormsModule,
+        RouterTestingModule,
+        HttpClientModule
+      ]
     })
     .compileComponents();
   }));
@@ -17,9 +44,15 @@ describe('HomeComponent', () => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    debugElement = fixture.debugElement.query(By.css('div[class=header]'));
+    htmlElement = debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should say Welcome to Tidder! on the homepage', () => {
+    expect(htmlElement.textContent).toEqual('Welcome to Tidder!');
+  })
 });
