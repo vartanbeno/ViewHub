d6d0c995f7969eb2f15d9d208d3c34f7cc8a206b
vartanbeno <vartanbeno@gmail.com>
2018-08-23 23:47:30 -0400

    #51 add tests for homepage welcome message

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
-import { DebugElement } from '@angular/core';
+import { DebugElement, Renderer } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { AddPostComponent } from '../add-post/add-post.component';
 import { ListOfPostsComponent } from '../list-of-posts/list-of-posts.component';
@@ -9,16 +9,17 @@ import { LoaderComponent } from '../loader/loader.component';
 import { ErrorMessageComponent } from '../error-message/error-message.component';
 import { EditPostComponent } from '../edit-post/edit-post.component';
 import { DeletePostComponent } from '../delete-post/delete-post.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../../services/auth.service';
 
-describe('HomeComponent', () => {
+fdescribe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
   let debugElement: DebugElement;
-  let htmlElement: HTMLElement;
+  let loggedOutHeader: HTMLElement;
+  let loggedInHeader: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -35,24 +36,69 @@ describe('HomeComponent', () => {
         FormsModule,
         RouterTestingModule,
         HttpClientModule
+      ],
+      providers: [
+        HomeComponent,
+        { provide: AuthService, useClass: MockAuthService },
+        Renderer
       ]
     })
     .compileComponents();
+
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    debugElement = fixture.debugElement.query(By.css('div[class=header]'));
-    htmlElement = debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should say Welcome to Tidder! on the homepage', () => {
-    expect(htmlElement.textContent).toEqual('Welcome to Tidder!');
+  describe('greeting message', () => {
+    it(`should say 'Welcome to Tidder!' on the homepage if user is not logged in`, () => {
+      debugElement = fixture.debugElement.query(By.css('div[class=header]'));
+      loggedOutHeader = debugElement.nativeElement;
+      expect(loggedOutHeader.textContent).toEqual('Welcome to Tidder!');
+    })
+
+    it(`should say 'Hello, [name]' on the homepage if user is logged in`, () => {
+      localStorage.setItem('token', 'token');
+      localStorage.setItem('name', 'test');
+      localStorage.setItem('id', '1');
+
+      fixture.detectChanges();
+
+      debugElement = fixture.debugElement.query(By.css('h2'));
+      loggedInHeader = debugElement.nativeElement;
+
+      expect(loggedInHeader.textContent).toContain('Hello, test');
+    })
+  })
+
+  afterEach(() => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('name');
+    localStorage.removeItem('id');
   })
 });
+
+class MockAuthService {  
+  loggedIn() {
+    return !!(this.getToken() && this.getName() && this.getId());
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  getName() {
+    return localStorage.getItem('name');
+  }
+
+  getId() {
+    return localStorage.getItem('id');
+  }
+}
