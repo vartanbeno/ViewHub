6eb5a85659fb91b7a6da71e9b22ba2d3de77d333
vartanbeno <vartanbeno@gmail.com>
2018-09-04 23:39:06 -0400

    #51 some more unit tests

--- src/app/components/add-comment/add-comment.component.spec.ts
+++ src/app/components/add-comment/add-comment.component.spec.ts
@@ -4,10 +4,14 @@ import { AddCommentComponent } from './add-comment.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AddCommentComponent', () => {
   let component: AddCommentComponent;
   let fixture: ComponentFixture<AddCommentComponent>;
+
+  let submitButton: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,9 +29,22 @@ describe('AddCommentComponent', () => {
     fixture = TestBed.createComponent(AddCommentComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    submitButton = fixture.debugElement.query(By.css('button[type=submit]'));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call the addComment method when the submit button is clicked', () => {
+    component.comment.body = 'test comment';
+    fixture.detectChanges();
+
+    spyOn(component, 'addComment');
+    submitButton.nativeElement.click();
+
+    fixture.whenStable().then(() => {
+      expect(component.addComment).toHaveBeenCalled();
+    })
+  })
 });
--- src/app/components/create-view/create-view.component.spec.ts
+++ src/app/components/create-view/create-view.component.spec.ts
@@ -4,10 +4,14 @@ import { CreateViewComponent } from './create-view.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('CreateViewComponent', () => {
   let component: CreateViewComponent;
   let fixture: ComponentFixture<CreateViewComponent>;
+
+  let createButton: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,9 +29,32 @@ describe('CreateViewComponent', () => {
     fixture = TestBed.createComponent(CreateViewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    createButton = fixture.debugElement.query(By.css('button[type=submit]'));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call createView method when submit button is clicked', () => {
+    component.viewData.name = 'testname';
+    component.viewData.description = 'test description';
+
+    fixture.detectChanges();
+
+    spyOn(component,'createView');
+    createButton.nativeElement.click();
+
+    fixture.whenStable().then(() => {
+      expect(component.createView).toHaveBeenCalled();
+    })
+  })
+
+  it(`should display error message if the view name is invalid`, () => {
+    component.nameInvalid = true;
+    fixture.detectChanges();
+
+    let errorMessage: DebugElement = fixture.debugElement.query(By.css('#error-message'));
+    expect(errorMessage).toBeTruthy();
+  })
 });
