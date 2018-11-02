4d0d2d5635c8eda87934290ac3814d581c3be115
vartanbeno <vartanbeno@gmail.com>
2018-11-02 09:01:28 -0400

    Add tslint yml file
    Signed-off-by: vartanbeno <vartanbeno@gmail.com>

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -10,8 +10,7 @@ export class AuthGuard implements CanActivate {
   canActivate(): boolean {
     if (this.authService.loggedIn()) {
       return true;
-    }
-    else {
+    } else {
       this.router.navigate(['login']);
     }
   }
